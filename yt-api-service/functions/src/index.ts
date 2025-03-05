dd1e291b2bcc3367eefa31a8567b1ace7de63e3b
ekim611 <ekim611@gatech.edu>
2025-03-05 03:03:21 -0500

    Added function into api service

--- yt-api-service/functions/src/index.ts
+++ yt-api-service/functions/src/index.ts
@@ -13,6 +13,17 @@ const firestore = new Firestore({
 const storage = new Storage();
 const rawVideoBucketName = "ezrakimcs-ytb-raw-videos";
 
+const videoCollectionId = "videos";
+
+export interface Video {
+  id?: string,
+  uid?: string,
+  filename?: string,
+  status?: "processing" | "processed",
+  title?: string,
+  description?: string
+}
+
 export const createUser = functions.auth.user().onCreate((user) => {
   const userInfo = {
     uid: user.uid,
@@ -46,5 +57,11 @@ export const generateUploadUrl = onCall({maxInstances: 1}, async (request) => {
   });
 
   return {url, fileName};
+});
+
+export const getVideos = onCall({maxInstances: 1}, async () => {
+  const snapshot =
+        await firestore.collection(videoCollectionId).limit(10).get();
+  return snapshot.docs.map((doc) => doc.data());
 });
 
